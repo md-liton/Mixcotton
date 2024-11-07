3b2b8afd3be2c3f8a04b42545066df56e8e50c03
md-liton <mdliton5506@gmail.com>
2024-11-08 01:20:54 +0600

    Banner

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import banner from '../../assets/Banner.png'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+const Banner = () => {
+    var settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true, 
+        autoplaySpeed: 2000,
+      };
+  return (
+      <section >
+          <div className="banner_main overflow-hidden">
+              <Slider {...settings}>
+                  <div>
+                      <img src={banner} alt="img" className='w-full' />
+                  </div>
+                  <div>
+                      <img src={banner} alt="img" className='w-full' />
+                  </div>
+                  <div>
+                      <img src={banner} alt="img" className='w-full' />
+                  </div>
+              </Slider>
+          </div>
+      </section>
+  )
+}
+
+export default Banner
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
 import Header from '../Header/Header'
+import Banner from '../Banner/Banner'
 
 const Home = () => {
   return (
     <>
     <Navbar/>
     <Header/>
+    <Banner/>
     </>
   )
 }
