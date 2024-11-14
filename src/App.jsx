53c9c4e389347b0e991f8bd783a6771aa445f6df
md-liton <mdliton5506@gmail.com>
2024-11-14 23:43:57 +0600

    React-Router-Dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import { useState } from 'react'
 import Home from './Components/Home/Home'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Product from './Components/Product/Product';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+  {
+    path: "/product",
+    element: <Product/>,
+  },
+]);
 
 function App() {
 
   return (
     <>
-    <Home/>
+    <RouterProvider router={router} />
     </>
   )
 }
---
+++ src/Components/Product/Product.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Product = () => {
+  return (
+    <section>
+        <div className="container">
+            <div className="product_main">
+                <p>mdliton</p>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Product
