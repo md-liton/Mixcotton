e8f6a430c654885dd7c34f931f16d3ac9acec255
md-liton <mdliton5506@gmail.com>
2024-12-08 00:49:23 +0600

    Routes

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,18 @@ import {
 } from "react-router-dom";
 import Product from './Components/Product/Product';
 import Error from './Components/Error/Error';
+import Shop from './Components/Shop/Shop';
+import About from './Components/About/About';
+import Contacts from './Components/Contacts/Contacts';
+import Journal from './Components/Journal/Journal';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/"  element={<Home/>} >
+    <Route path="/shop"  element={<Shop/>} />
+    <Route path="/about"  element={<About/>} />
+    <Route path="/contacts"  element={<Contacts/>} />
+    <Route path="/journal"  element={<Journal/>} />
     <Route path="/products"  element={<Product/>} />
     <Route path="*"  element={<Error/>} />
     </Route>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import logo from '../../assets/Logo.png'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
@@ -11,11 +12,21 @@ const Navbar = () => {
                 </div>
                 <div className="menu md:w-[80%]">
                     <ul className='md:text-[18px] font-dm md:flex md:justify-center md:items-center gap-[30px]'>
-                        <li className=' text-secondary hover:text-primary font-bold duration-300 cursor-pointer'>Home</li>
-                        <li className=' text-secondary hover:text-primary font-bold duration-300 cursor-pointer'>Shop</li>
-                        <li className=' text-secondary hover:text-primary font-bold duration-300 cursor-pointer'>About</li>
-                        <li className=' text-secondary hover:text-primary font-bold duration-300 cursor-pointer'>Contacts</li>
-                        <li className=' text-secondary hover:text-primary font-bold duration-300 cursor-pointer'>Journal</li>
+                        <li className=' text-secondary hover:text-primary font-bold duration-300 cursor-pointer'>
+                            <Link to='/'>Home</Link>
+                        </li>
+                        <li className=' text-secondary hover:text-primary font-bold duration-300 cursor-pointer'>
+                            <Link to='/shop'>Shop</Link>
+                        </li>
+                        <li className=' text-secondary hover:text-primary font-bold duration-300 cursor-pointer'>
+                            <Link to='/about'>About</Link>
+                        </li>
+                        <li className=' text-secondary hover:text-primary font-bold duration-300 cursor-pointer'>
+                            <Link to='/contacts'>Contacts</Link>
+                        </li>
+                        <li className=' text-secondary hover:text-primary font-bold duration-300 cursor-pointer'>
+                            <Link to='journal'>Journal</Link>
+                        </li>
                     </ul>
                 </div>
             </div>
