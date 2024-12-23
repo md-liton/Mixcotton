761a5c32148673082fab2868c8ec6b88a2a70737
md-liton <mdliton5506@gmail.com>
2024-12-23 19:44:53 +0600

    Button-Modify

--- src/Components/SpecialOffer/Special.jsx
+++ src/Components/SpecialOffer/Special.jsx
@@ -14,8 +14,8 @@ const Special = () => {
                           </div>
                           <div className='new_btn absolute bottom-0 left-0 w-[370px] ease-in duration-300  bg-[#F5F5F3]  text-white font-normal font-dm rounded-lg'>
                               <div className=' py-[40px] px-[20px] flex justify-center items-center gap-[20px] '>
-                                  <button className='py-[10px] px-[30px] rounded-lg bg-primary hover:bg-transparent hover:border hove:border-[1px] hover:border-primary hover:text-primary easy-in duration-300 border border-[1px] border-primary'>Add to Cart</button>
-                                  <button className='py-[10px] px-[30px] rounded-lg bg-primary hover:bg-transparent hover:border hove:border-[1px] hover:border-primary hover:text-primary easy-in duration-300 border border-[1px] border-primary'>View Details</button>
+                                  <button className='py-[10px] px-[30px]  bg-primary hover:bg-transparent hover:border hove:border-[1px] hover:border-primary hover:text-primary easy-in duration-300 border border-[1px] border-primary'>Add to Cart</button>
+                                  <button className='py-[10px] px-[30px]  bg-primary hover:bg-transparent hover:border hove:border-[1px] hover:border-primary hover:text-primary easy-in duration-300 border border-[1px] border-primary'>View Details</button>
                               </div>
                           </div>
                       </div>
@@ -31,8 +31,8 @@ const Special = () => {
                           </div>
                           <div className='new_btn absolute bottom-0 left-0 w-[370px] ease-in duration-300  bg-[#F5F5F3]  text-white font-normal font-dm rounded-lg'>
                               <div className=' py-[40px] px-[20px] flex justify-center items-center gap-[20px] '>
-                                  <button className='py-[10px] px-[30px] rounded-lg bg-primary hover:bg-transparent hover:border hove:border-[1px] hover:border-primary hover:text-primary easy-in duration-300 border border-[1px] border-primary'>Add to Cart</button>
-                                  <button className='py-[10px] px-[30px] rounded-lg bg-primary hover:bg-transparent hover:border hove:border-[1px] hover:border-primary hover:text-primary easy-in duration-300 border border-[1px] border-primary'>View Details</button>
+                                  <button className='py-[10px] px-[30px]  bg-primary hover:bg-transparent hover:border hove:border-[1px] hover:border-primary hover:text-primary easy-in duration-300 border border-[1px] border-primary'>Add to Cart</button>
+                                  <button className='py-[10px] px-[30px]  bg-primary hover:bg-transparent hover:border hove:border-[1px] hover:border-primary hover:text-primary easy-in duration-300 border border-[1px] border-primary'>View Details</button>
                               </div>
                           </div>
                       </div>
@@ -48,8 +48,59 @@ const Special = () => {
                           </div>
                           <div className='new_btn absolute bottom-0 left-0 w-[370px] ease-in duration-300  bg-[#F5F5F3]  text-white font-normal font-dm rounded-lg'>
                               <div className=' py-[40px] px-[20px] flex justify-center items-center gap-[20px] '>
-                                  <button className='py-[10px] px-[30px] rounded-lg bg-primary hover:bg-transparent hover:border hove:border-[1px] hover:border-primary hover:text-primary easy-in duration-300 border border-[1px] border-primary'>Add to Cart</button>
-                                  <button className='py-[10px] px-[30px] rounded-lg bg-primary hover:bg-transparent hover:border hove:border-[1px] hover:border-primary hover:text-primary easy-in duration-300 border border-[1px] border-primary'>View Details</button>
+                                  <button className='py-[10px] px-[30px]  bg-primary hover:bg-transparent hover:border hove:border-[1px] hover:border-primary hover:text-primary easy-in duration-300 border border-[1px] border-primary'>Add to Cart</button>
+                                  <button className='py-[10px] px-[30px]  bg-primary hover:bg-transparent hover:border hove:border-[1px] hover:border-primary hover:text-primary easy-in duration-300 border border-[1px] border-primary'>View Details</button>
+                              </div>
+                          </div>
+                      </div>
+                      <div className='flex justify-between items-center gap-[30px] py-[20px] px-[10px] font-dm w-[370px] '>
+                          <span className='text-[20px] font-bold  text-primary'>Basic Crew Neck Tee</span>
+                          <span className='text-[15px] font-normal text-secondary'>$50</span>
+                      </div>
+                  </div>
+                  <div>
+                      <div className='relative new_product cursor-pointer'>
+                          <div className='w-[370px] h-[370px] rounded-lg overflow-hidden '>
+                              <img src={newimg} alt="img" className='w-full h-full' />
+                          </div>
+                          <div className='new_btn absolute bottom-0 left-0 w-[370px] ease-in duration-300  bg-[#F5F5F3]  text-white font-normal font-dm rounded-lg'>
+                              <div className=' py-[40px] px-[20px] flex justify-center items-center gap-[20px] '>
+                                  <button className='py-[10px] px-[30px]  bg-primary hover:bg-transparent hover:border hove:border-[1px] hover:border-primary hover:text-primary easy-in duration-300 border border-[1px] border-primary'>Add to Cart</button>
+                                  <button className='py-[10px] px-[30px]  bg-primary hover:bg-transparent hover:border hove:border-[1px] hover:border-primary hover:text-primary easy-in duration-300 border border-[1px] border-primary'>View Details</button>
+                              </div>
+                          </div>
+                      </div>
+                      <div className='flex justify-between items-center gap-[30px] py-[20px] px-[10px] font-dm w-[370px] '>
+                          <span className='text-[20px] font-bold  text-primary'>Basic Crew Neck Tee</span>
+                          <span className='text-[15px] font-normal text-secondary'>$50</span>
+                      </div>
+                  </div>
+                  <div>
+                      <div className='relative new_product cursor-pointer'>
+                          <div className='w-[370px] h-[370px] rounded-lg overflow-hidden '>
+                              <img src={newimg} alt="img" className='w-full h-full' />
+                          </div>
+                          <div className='new_btn absolute bottom-0 left-0 w-[370px] ease-in duration-300  bg-[#F5F5F3]  text-white font-normal font-dm rounded-lg'>
+                              <div className=' py-[40px] px-[20px] flex justify-center items-center gap-[20px] '>
+                                  <button className='py-[10px] px-[30px]  bg-primary hover:bg-transparent hover:border hove:border-[1px] hover:border-primary hover:text-primary easy-in duration-300 border border-[1px] border-primary'>Add to Cart</button>
+                                  <button className='py-[10px] px-[30px]  bg-primary hover:bg-transparent hover:border hove:border-[1px] hover:border-primary hover:text-primary easy-in duration-300 border border-[1px] border-primary'>View Details</button>
+                              </div>
+                          </div>
+                      </div>
+                      <div className='flex justify-between items-center gap-[30px] py-[20px] px-[10px] font-dm w-[370px] '>
+                          <span className='text-[20px] font-bold  text-primary'>Basic Crew Neck Tee</span>
+                          <span className='text-[15px] font-normal text-secondary'>$50</span>
+                      </div>
+                  </div>
+                  <div>
+                      <div className='relative new_product cursor-pointer'>
+                          <div className='w-[370px] h-[370px] rounded-lg overflow-hidden '>
+                              <img src={newimg} alt="img" className='w-full h-full' />
+                          </div>
+                          <div className='new_btn absolute bottom-0 left-0 w-[370px] ease-in duration-300  bg-[#F5F5F3]  text-white font-normal font-dm rounded-lg'>
+                              <div className=' py-[40px] px-[20px] flex justify-center items-center gap-[20px] '>
+                                  <button className='py-[10px] px-[30px]  bg-primary hover:bg-transparent hover:border hove:border-[1px] hover:border-primary hover:text-primary easy-in duration-300 border border-[1px] border-primary'>Add to Cart</button>
+                                  <button className='py-[10px] px-[30px]  bg-primary hover:bg-transparent hover:border hove:border-[1px] hover:border-primary hover:text-primary easy-in duration-300 border border-[1px] border-primary'>View Details</button>
                               </div>
                           </div>
                       </div>
