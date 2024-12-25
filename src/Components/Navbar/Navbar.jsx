2e60812c7f58109802a893f07608b87fc8c39c91
md-liton <mdliton5506@gmail.com>
2024-12-25 15:23:10 +0600

    Navbar-Active

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import logo from '../../assets/Logo.png'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 const Navbar = () => {
+    const location = useLocation()
   return (
     <nav className='py-[25px]'>
         <div className="container">
@@ -14,17 +15,17 @@ const Navbar = () => {
                 </div>
                 <div className="menu md:w-[80%]">
                     <ul className='md:text-[18px] font-dm md:flex md:justify-center md:items-center gap-[30px]'>
-                        <li className=' text-secondary hover:text-primary font-bold duration-300 cursor-pointer'>
-                            <Link to='/'>Home</Link>
+                        <li >
+                            <Link to='/' className={` ${location.pathname == '/' ? 'text-primary' : 'text-secondary'}  hover:text-primary font-bold duration-300 cursor-pointer `}>Home</Link>
                         </li>
-                        <li className=' text-secondary hover:text-primary font-bold duration-300 cursor-pointer'>
-                            <Link to='/shop'>Shop</Link>
+                        <li >
+                            <Link to='/shop' className={` ${location.pathname == '/shop' ? 'text-primary' : 'text-secondary'}  hover:text-primary font-bold duration-300 cursor-pointer `}>Shop</Link>
                         </li>
-                        <li className=' text-secondary hover:text-primary font-bold duration-300 cursor-pointer'>
-                            <Link to='/about'>About</Link>
+                        <li >
+                            <Link to='/about' className={` ${location.pathname == '/about' ? 'text-primary' : 'text-secondary'}  hover:text-primary font-bold duration-300 cursor-pointer `}>About</Link>
                         </li>
-                        <li className=' text-secondary hover:text-primary font-bold duration-300 cursor-pointer'>
-                            <Link to='/contacts'>Contacts</Link>
+                        <li >
+                            <Link to='/contacts' className={` ${location.pathname == '/contacts' ? 'text-primary' : 'text-secondary'}  hover:text-primary font-bold duration-300 cursor-pointer `}>Contacts</Link>
                         </li>
                     </ul>
                 </div>
