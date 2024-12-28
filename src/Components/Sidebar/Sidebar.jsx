26a9dc3daf07b47e64ffbcc366b3f2ee97a0c6b5
md-liton <mdliton5506@gmail.com>
2024-12-28 14:34:25 +0600

    ShopByCatagory-update

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,23 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaPlus } from "react-icons/fa6";
+import { Link } from 'react-router-dom';
+import { FaLongArrowAltRight } from "react-icons/fa";
+import { FaMinus } from "react-icons/fa6";
+
+
 
 
 const Sidebar = () => {
+    const [men,setMen]=useState(false)
+    const [women,setWomen]=useState(false)
   return (
     <section>
             <div className="sidebar_main font-dm">
                 <div>
                     <h1 className='text-[20px] font-bold  text-primary'>Shop by Category</h1>
                     <ul className='py-[20px]'>
-                        <li className='flex justify-between  items-center cursor-pointer py-[10px] text-[15px] font-normal text-secondary  border-b-[2px] boder-secondary'><span>Category 1</span> <span><FaPlus /></span>
+                        <li onClick={()=>setMen(!men)}>
+                            <Link to='' className={`flex justify-between  items-center cursor-pointer py-[10px] text-[15px] ${!men ? 'font-normal' : 'font-bold'} text-secondary  border-b-[2px] boder-secondary duration-300`}><span>Men</span> {!men ? <span><FaPlus /></span>  : <span><FaMinus /></span> }</Link>
                         </li>
-                        <li className='flex justify-between items-center cursor-pointer py-[10px] text-[15px] font-normal text-secondary  border-b-[2px] boder-secondary'><span>Category 2</span> <span></span>
+                      <div className={`border-b-[2px] boder-secondary ${!men && 'hidden'} duration-300`}>
+                          <ul className='px-[15px]'>
+                              <li className='flex items-center gap-[10px] cursor-pointer py-[10px] text-[15px] font-normal text-secondary  '> <span><FaLongArrowAltRight /></span><span>Shirt</span>
+                              </li>
+                              <li className='flex items-center gap-[10px] cursor-pointer py-[10px] text-[15px] font-normal text-secondary  '> <span><FaLongArrowAltRight /></span><span>T-shirt</span>
+                              </li>
+                              <li className='flex items-center gap-[10px] cursor-pointer py-[10px] text-[15px] font-normal text-secondary  '> <span><FaLongArrowAltRight /></span><span>Pant</span>
+                              </li>
+                              <li className='flex items-center gap-[10px] cursor-pointer py-[10px] text-[15px] font-normal text-secondary  '> <span><FaLongArrowAltRight /></span><span>Panjabi</span>
+                              </li>
+                              <li className='flex items-center gap-[10px] cursor-pointer py-[10px] text-[15px] font-normal text-secondary  '> <span><FaLongArrowAltRight /></span><span>Winter Special</span>
+                              </li>
+                          </ul>
+                      </div>
+                      <li onClick={()=>setWomen(!women)}>
+                            <Link to='' className={`flex justify-between  items-center cursor-pointer py-[10px] text-[15px] ${!women ? 'font-normal' : 'font-bold'} text-secondary  border-b-[2px] boder-secondary duration-300`}><span>Women</span> {!women ? <span><FaPlus /></span>  : <span><FaMinus /></span> }</Link>
                         </li>
-                        <li className='flex justify-between items-center cursor-pointer py-[10px] text-[15px] font-normal text-secondary  border-b-[2px] boder-secondary'><span>Category 3</span> <span><FaPlus /></span>
-                        </li>
-                        <li className='flex justify-between items-center cursor-pointer py-[10px] text-[15px] font-normal text-secondary  border-b-[2px] boder-secondary'><span>Category 4</span> <span></span>
-                        </li>
-                        <li className='flex justify-between items-center cursor-pointer py-[10px] text-[15px] font-normal text-secondary  border-b-[2px] boder-secondary'><span>Category 5</span> <span></span>
+                      <div className={`border-b-[2px] boder-secondary ${!women && 'hidden'} duration-300`}>
+                          <ul className='px-[15px]'>
+                              <li className='flex items-center gap-[10px] cursor-pointer py-[10px] text-[15px] font-normal text-secondary  '> <span><FaLongArrowAltRight /></span><span>Burka</span>
+                              </li>
+                              <li className='flex items-center gap-[10px] cursor-pointer py-[10px] text-[15px] font-normal text-secondary  '> <span><FaLongArrowAltRight /></span><span>Hijab</span>
+                              </li>
+                              <li className='flex items-center gap-[10px] cursor-pointer py-[10px] text-[15px] font-normal text-secondary  '> <span><FaLongArrowAltRight /></span><span>Sharee</span>
+                              </li>
+                              <li className='flex items-center gap-[10px] cursor-pointer py-[10px] text-[15px] font-normal text-secondary  '> <span><FaLongArrowAltRight /></span><span>Three pics</span>
+                              </li>
+                              <li className='flex items-center gap-[10px] cursor-pointer py-[10px] text-[15px] font-normal text-secondary  '> <span><FaLongArrowAltRight /></span><span>Winter Special</span>
+                              </li>
+                          </ul>
+                      </div>
+                        <li>
+                            <Link to='' className='flex justify-between  items-center cursor-pointer py-[10px] text-[15px] font-normal text-secondary  border-b-[2px] boder-secondary'><span>Baby</span> <span><FaPlus /></span></Link>
                         </li>
                     </ul>
                 </div>
